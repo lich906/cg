e4a8a3e9ff30f0a68d7203465f7f499a1ef00a2e
AndreyM <222mushkin@gmail.com>
2023-03-11 19:51:56 +0300

    1.1 настроил матрицу проецирования, которая сохраняет пропорции изображения при изменении размеров видового порта

--- lab1/task1/main.cpp
+++ lab1/task1/main.cpp
@@ -23,11 +23,33 @@ private:
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glViewport(0, 0, width, height);
+		SetupProjectionMatrix(width, height);
 
 		m_letterM.Draw(-0.6f, 0.0f);
 		m_letterA.Draw(-0.3f, 0.0f);
 		m_letterA2.Draw(0.0f, 0.0f);
 	}
+
+	static void SetupProjectionMatrix(int width, int height)
+	{
+		// Вычисляет матрицу ортографического преобразования такую, чтобы вписать квадратную область
+		// [-1;+1] по обеим осям внутрь видового порта размером width*height пикселей
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		const double aspectRatio = double(width) / double(height);
+		double viewWidth = 3.0;
+		double viewHeight = viewWidth;
+		if (aspectRatio > 1.0)
+		{
+			viewWidth = viewHeight * aspectRatio;
+		}
+		else
+		{
+			viewHeight = viewWidth / aspectRatio;
+		}
+		glOrtho(-viewWidth * 0.2, viewWidth * 0.2, -viewHeight * 0.2, viewHeight * 0.2, -1.0, 1.0);
+	}
+
 
 	LetterM m_letterM;
 	LetterA m_letterA;
